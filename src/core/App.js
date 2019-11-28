11336c146b92a1a290ac73507b7716a81f29800c
Overrideveloper <bansowisdom@gmail.com>
2019-11-28 20:40:56 +0000

    feat(core): Initial Commit

---
+++ src/core/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <header className="app-header">
+                    React Javascript Webpack Boilerplate
+                </header>
+            </div>
+        )
+    }
+}
+
+export default App;
